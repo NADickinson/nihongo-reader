1b561f808a9e43b14d37f4bd2872560f96d6bff5
Nathan Dickinson <nathan.a.dickinson@gmail.com>
2023-09-04 14:32:01 +0100

    now find instead of filter+ second story data

--- src/data/data.mjs
+++ src/data/data.mjs
@@ -251,6 +251,219 @@ export const introStoryArray = [
   { word: 'Nathan', id: 44 },
 ]
 
+export const proverbsArray = [
+  { word: '一', reading: 'いち', meaning: 'One', type: 'Noun', id: 1 },
+  { word: ':', id: 2 },
+  { word: '十', reading: 'じゅう', meaning: 'Ten', type: 'Noun', id: 3 },
+  {
+    word: '人',
+    reading: 'にん',
+    meaning: 'Person, people',
+    type: 'Noun',
+    id: 4,
+  },
+  { word: '十', reading: 'と', meaning: 'Ten', type: 'Noun', id: 5 },
+  { word: '色', reading: 'いろ', meaning: 'Colour', type: 'Noun', id: 6 },
+
+  { word: '二', reading: 'に', meaning: 'Two', type: 'Noun', id: 7 },
+  {
+    word: '弱',
+    reading: 'じゃく',
+    meaning: 'Weak',
+    type: 'Adjective',
+    id: 8,
+  },
+  { word: '肉', reading: 'にく', meaning: 'Meat', type: 'Noun', id: 9 },
+  {
+    word: '強',
+    reading: 'きょう',
+    meaning: 'Strong',
+    type: 'Adjective',
+    id: 10,
+  },
+  { word: '食', reading: 'しょく', meaning: 'Ear', type: 'Verb', id: 11 },
+
+  { word: '三', reading: 'さん', meaning: '3', type: 'Noun', id: 12 },
+  {
+    word: '異体',
+    reading: 'いたい',
+    meaning: 'Different body',
+    type: 'Noun',
+    id: 13,
+  },
+  {
+    word: '同心',
+    reading: 'どうしん',
+    meaning: 'Same heart',
+    type: 'Adjective',
+    id: 14,
+  },
+
+  { word: '四', reading: 'よん', meaning: 'Four', type: 'Noun', id: 15 },
+  { word: '自', reading: 'じ', meaning: 'Oneself', type: 'Noun', id: 16 },
+  {
+    word: '業',
+    reading: 'ごう',
+    meaning: 'Business, Performance',
+    type: 'Noun',
+    id: 17,
+  },
+  {
+    word: '自得',
+    reading: 'じとく',
+    meaning: 'Being paid back for ones deeds',
+    type: 'Set phrase',
+    id: 18,
+  },
+
+  { word: '五', reading: 'ご', meaning: 'Five', type: 'Noun', id: 19 },
+  {
+    word: '虎穴',
+    reading: 'こけつ',
+    meaning: 'Tigers den',
+    type: 'Noun',
+    id: 20,
+  },
+  { word: 'に', meaning: 'Direction, at, to', type: 'Grammar', id: 21 },
+  {
+    word: '入らずんば',
+    reading: 'いらずんば',
+    meaning: 'If you dont enter',
+    type: 'Verb',
+    id: 22,
+  },
+  {
+    word: '虎子',
+    reading: 'こじ',
+    meaning: 'Tiger cub',
+    type: 'Noun',
+    id: 23,
+  },
+
+  {
+    word: 'を',
+    meaning: 'Shows the direct object of action',
+    type: 'Grammar',
+    id: 24,
+  },
+  {
+    word: '得ず',
+    reading: 'えず',
+    meaning: 'Wont gain',
+    type: 'Verb',
+    id: 25,
+  },
+
+  { word: '六', reading: 'ろく', meaning: 'Six', type: 'Noun', id: 26 },
+  { word: '猿', reading: 'さる', meaning: 'Monkey', type: 'Noun', id: 27 },
+  { word: 'も', meaning: 'Even, also', type: 'Grammar', id: 28 },
+  { word: '木', reading: 'き', meaning: 'Tree', type: 'Noun', id: 29 },
+  { word: 'から', meaning: 'From, because', type: 'Grammar', id: 30 },
+  {
+    word: '落ちる',
+    reading: 'おちる',
+    meaning: 'To fall',
+    type: 'Verb',
+    id: 31,
+  },
+
+  { word: '七', reading: 'しち', meaning: 'Seven', type: 'Noun', id: 32 },
+  { word: '猫', reading: 'ねこ', meaning: 'Cat', type: 'Noun', id: 33 },
+  {
+    word: '小判',
+    reading: 'こばん',
+    meaning: 'Gold coins',
+    type: 'Noun',
+    id: 34,
+  },
+
+  { word: '八', reading: 'はち', meaning: 'Eight', type: 'Noun', id: 35 },
+  { word: '兎', reading: 'うさぎ', meaning: 'Rabbit', type: 'Noun', id: 36 },
+  { word: '追う', reading: 'おう', meaning: 'Chase', type: 'Verb', id: 37 },
+  { word: '者', reading: 'もの', meaning: 'Person', type: 'Noun', id: 38 },
+  { word: 'は', meaning: 'Topic marking particle', type: 'Grammar', id: 39 },
+
+  { word: '九', reading: 'きゅう', meaning: 'Nine', type: 'Noun', id: 40 },
+  {
+    word: '転び',
+    reading: 'ころび',
+    meaning: 'To fall',
+    type: 'Verb',
+    id: 41,
+  },
+  {
+    word: '起き',
+    reading: 'おき',
+    meaning: 'To rise, to get up',
+    type: 'Verb',
+    id: 42,
+  },
+
+  { word: '十', reading: 'じゅう', meaning: 'Ten', type: 'Noun', id: 43 },
+  { word: '花', reading: 'はな', meaning: 'Flower', type: 'Noun', id: 44 },
+  { word: 'より', meaning: 'Except, other than', type: 'Grammar', id: 45 },
+  {
+    word: '団子',
+    reading: 'だんご',
+    meaning: 'Dumpling',
+    type: 'Noun',
+    id: 46,
+  },
+
+  {
+    word: '十一',
+    reading: 'じゅういち',
+    meaning: 'Eleven',
+    type: 'Noun',
+    id: 47,
+  },
+  {
+    word: '知らぬ',
+    reading: 'しらぬ',
+    meaning: 'Not knowing',
+    type: 'Verb',
+    id: 48,
+  },
+  { word: 'が', meaning: 'Subject marking particle', type: 'Grmmar', id: 49 },
+  { word: '仏', reading: 'ほとけ', meaning: 'Buddha', type: 'Noun', id: 50 },
+
+  {
+    word: '十二 ',
+    reading: 'じゅうに',
+    meaning: 'Twelve',
+    type: 'Noun',
+    id: 51,
+  },
+  {
+    word: '覆',
+    reading: 'ふく',
+    meaning: 'Capsize, to be ruined',
+    type: 'Adjective',
+    id: 52,
+  },
+  { word: '水', reading: 'すい', meaning: 'Water', type: 'Noun', id: 53 },
+  {
+    word: '盆',
+    reading: 'ぼん',
+    meaning: 'Tray, basin',
+    type: 'Noun',
+    id: 54,
+  },
+  {
+    word: '帰らず',
+    reading: 'かえらず',
+    meaning: 'To not return',
+    type: 'Verb',
+    id: 55,
+  },
+]
+
+// 一: 十人十色 , 二: 弱肉強食, 三: 異体同心,
+//四: 自業自得, 五: 虎穴に入らずんば虎子を得ず,
+//六: 猿も木から落ちる, 七: 猫に小判, 八: 二兎を追う者は一兎をも得ず,
+//九: 七転び八起き, 十: 花より団子,
+//十一: 知らぬが仏, 十二: 覆水盆に帰らず
+
 // はじめまして Nathan です。よろしくお願いします。
 // 僕の趣味は日本語勉強です。何人かによるとこの趣味ちょっとつまらないそうです。
 // でも、僕の毎日勉強することは本当に楽しいです。
@@ -264,20 +477,30 @@ const introStoryArrayOfIds = [
   28, 34, 18, 19, 2, 12, 32, 25, 35, 36, 37, 38, 5, 35, 7, 39, 40, 15, 16, 41,
   42, 43,
 ]
-//TODO: make below find not filter
+
+const proverbsArrayOfIds = [
+  1, 2, 3, 4, 5, 6, 7, 2, 8, 9, 10, 11, 12, 2, 13, 14, 15, 2, 16, 17, 18, 19, 2,
+  20, 21, 22, 23, 24, 25, 26, 2, 27, 28, 29, 30, 31, 32, 2, 33, 21, 34, 35, 2,
+  7, 36, 24, 37, 38, 39, 1, 36, 24, 28, 25, 40, 2, 32, 41, 35, 42, 43, 2, 44,
+  45, 46, 47, 2, 48, 49, 50, 51, 2, 52, 53, 54, 21, 55,
+]
 
 const mappedIdArray = (idArray, storyArray) => {
-  return idArray.map((id) => {
-    const orderedArray = storyArray.filter((obj) => {
-      if (obj.id === id) {
-        return obj
-      } else return false
+  const finalArray = idArray.map((id) => {
+    const foundObject = storyArray.find((obj) => {
+      return obj.id === id
     })
-    return orderedArray[0]
+    return foundObject
   })
+  return finalArray
 }
 
 export const introStoryFinal = mappedIdArray(
   introStoryArrayOfIds,
   introStoryArray
+)
+
+export const ProverbStoryFinal = mappedIdArray(
+  proverbsArrayOfIds,
+  proverbsArray
 )
