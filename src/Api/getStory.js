4672d9dce1fad0d81fdfb41e1643c236662aac5c
Nathan Dickinson <nathan.a.dickinson@gmail.com>
2023-09-07 13:58:53 +0100

    about description, now data anon

--- src/Api/getStory.js
+++ src/Api/getStory.js
@@ -1,8 +1,15 @@
 import { ProverbStoryFinal, introStoryFinal } from '../data/data.mjs'
 
 const storiesById = {
-  1: introStoryFinal,
-  2: ProverbStoryFinal,
+  1: {
+    story: introStoryFinal,
+    description:
+      'This is an intro about the author (Nathan), detailing his passions and goals in his life, I hope you enjoy!',
+  },
+  2: {
+    story: ProverbStoryFinal,
+    description: 'This is a list of some grear Japanese proverbs! enjoy!',
+  },
 }
 
 export const getStory = (id) => {
--- src/components/StoryPage.jsx
+++ src/components/StoryPage.jsx
@@ -5,26 +5,17 @@ import { ColourChart } from '../components/ColourChart'
 import { SideBar } from '../components/SideBar'
 import { useState } from 'react'
 import { Story } from '../components/story'
-import { ProverbStoryFinal, introStoryFinal } from '../data/data.mjs'
 import { useParams } from 'react-router-dom'
 import { getStory } from '../Api/getStory'
-
-const getDescription = (storyId) => {
-  if (storyId === 'intro') {
-    return 'This is an intro about the author (Nathan), detailing his passions and goals in his life, I hope you enjoy!'
-  } else {
-    return 'This is a list of some grear Japanese proverbs! enjoy!'
-  }
-}
 
 export const StoryPage = () => {
   const [hasClicked, setHasClicked] = useState(false)
   const storyId = useParams().storyId
-  const story = getStory(storyId)
+  const { story, description } = getStory(storyId)
 
   return (
     <PageWrapper>
-      <PageHeader aboutDescription={getDescription(storyId)} />
+      <PageHeader aboutDescription={description} />
       <Column justify="center" align="center" flexGrow="1">
         <SideBar
           height={'321px'}
